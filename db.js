16f2a32312c472917d632f110e65f0d741019321
Scott Thomson <55478300+scott-thomson82@users.noreply.github.com>
2020-01-24 13:30:37 +1300

    update merge

--- db.js
+++ db.js
@@ -4,13 +4,9 @@ const connection = require('knex')(config)
 
 module.exports = {
   getMovies: getMovies,
-<<<<<<< HEAD
-  getChar: getChar, 
-  addCharacter: addCharacter,
-=======
   getChar: getChar,
-  getCharByMovieId: getCharByMovieId
->>>>>>> 358c9908edae82172a4ddce6a837680f2ed799eb
+  getCharByMovieId: getCharByMovieId,
+  addCharacter: addCharacter
 }
 
 function getMovies(db = connection) {
@@ -23,10 +19,12 @@ function getChar(id, db = connection) {
   .where('characters.id', id)
   .join('movies', 'movies.id', '=', 'characters.movie_id')
   .select('*', 'characters.name AS characters_name')
+}
 
 function getCharByMovieId(db = connection) {
   return db('movies')
     .join('characters', 'movies.id', 'characters.movie_id')
+}
 
 function addCharacter(body, db = connection){
   console.log(body)
